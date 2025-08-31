3e386022ed57cff1952d74d7d5bb2af72625d86f
RyanPrax <140210909+RyanPrax@users.noreply.github.com>
2025-08-31 13:07:47 -0500

    Added previously simple login page code

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,321 @@
 import 'package:flutter/material.dart';
+import 'widgets/custom_text_field.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const List<Map<String, dynamic>> userData = [
+  {
+      'email': 'family@guy.com',
+      'password': 'PeterGriffin123!',
+  },
+  {
+      'email': 'user2@gmail.com',
+      'password': 'Password1!',
+  }
+  ];
 
-  // This widget is the root of your application.
+
+const String emailPattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
+const String passwordPattern = r'(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#\$%\^&\*\(\)_\+=]).{8,32}';
+const String phoneNumberPattern = r'^\+?[1-9]\d{9,14}$';
+final emailRegex = RegExp(emailPattern);
+final passwordRegex = RegExp(passwordPattern);
+final phoneNumberRegex = RegExp(phoneNumberPattern);
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: LoginScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        backgroundColor: Colors.yellow,
+        title: Text(
+          "Login",
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomTextField(
+                controller: emailController,
+                labelText: "Email Address",
+                isRequired: true,
+              ),
+              CustomTextField(
+                controller: passwordController,
+                labelText: "Password",
+                isPassword: true,
+                isRequired: true,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      final emailAddress = emailController.text;
+
+                      final password = passwordController.text;
+                      if (!emailRegex.hasMatch(emailAddress)) {
+                        showDialog(
+                          context: context,
+                          builder: (context) => const AlertDialog(
+                            content: Text("Invalid email address!"),
+                          ),
+                        );
+                        return;
+                      }
+                      if (!passwordRegex.hasMatch(password)) {
+                        showDialog(
+                          context: context,
+                          builder: (context) => const AlertDialog(
+                            content: Text("Invalid password!"),
+                          ),
+                        );
+                        return;
+                      }
+
+                      final userExists = userData.any((user) =>
+                        user['email'] == emailAddress && user['password'] == password
+                      );
+                      
+                      // Successful login
+                      if (userExists) {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            content: Text("Sign in successful!\nEmail: $emailAddress"),
+                          ),
+                        );
+                      }
+                      // Unsuccessful login
+                      else {
+                        showDialog(
+                          context: context,
+                          builder: (context) => const AlertDialog(
+                            content: Text("Email or password incorrect."),
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text("Sign In"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const RegisterScreen()),
+                    );
+                    },
+                    child: const Text("Register"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
     );
   }
+}
+
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController phoneNumberController = TextEditingController();
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
+
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    phoneNumberController.dispose();
+    firstNameController.dispose();
+    lastNameController.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        title: Text(
+          "Register",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          CustomTextField(
+            controller: firstNameController,
+            labelText: "First Name",
+            isRequired: true,
+          ),
+        CustomTextField(
+            controller: lastNameController,
+            labelText: "Last Name",
+            isRequired: true,
+          ),
+          CustomTextField(
+            controller: phoneNumberController,
+            labelText: "Phone Number",
+            isRequired: true,
+          ),
+          CustomTextField(
+            controller: emailController,
+            labelText: "Email Address",
+            isRequired: true,
+          ),
+        CustomTextField(
+            controller: passwordController,
+            labelText: "Password",
+            isRequired: true,
+            isPassword: true,
+          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                final firstName = firstNameController.text;
+                final lastName = lastNameController.text;
+                final phoneNumber = phoneNumberController.text;
+                final emailAddress = emailController.text;
+                final password = passwordController.text;
+
+                if (firstName == '') {
+                      showDialog(
+                        context: context,
+                        builder: (context) => const AlertDialog(
+                          content: Text("You are required a first name!"),
+                        ),
+                      );
+                      return;
+                }
+                if (lastName == '') {
+                      showDialog(
+                        context: context,
+                        builder: (context) => const AlertDialog(
+                          content: Text("You are required a last name!"),
+                        ),
+                      );
+                      return;
+                }
+                // TODO validate valid phone number
+                if (phoneNumber == '') {
+                      showDialog(
+                        context: context,
+                        builder: (context) => const AlertDialog(
+                          content: Text("You are required a phone number"),
+                        ),
+                      );
+                      return;
+                }
+                // Validate regex for email password and phone number
+                if (!emailRegex.hasMatch(emailAddress)) {
+                      showDialog(
+                        context: context,
+                        builder: (context) => const AlertDialog(
+                          content: Text("Invalid email address!"),
+                        ),
+                      );
+                      return;
+                }
+                if (!passwordRegex.hasMatch(password)) {
+                  showDialog(
+                    context: context,
+                    builder: (context) => const AlertDialog(
+                      content: Text("Invalid password!"),
+                    ),
+                  );
+                  return;
+                }
+                if (!phoneNumberRegex.hasMatch(phoneNumber)) {
+                  showDialog(
+                        context: context,
+                        builder: (context) => const AlertDialog(
+                          content: Text("Invalid phone number!"),
+                        ),
+                      );
+                      return;
+                }
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: const Text("Registration Info"),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("First Name: $firstName"),
+                        Text("Last Name: $lastName"),
+                        Text("Phone Number: $phoneNumber"),
+                        Text("Email Address: $emailAddress"),
+                        Text("Password: $password"),
+                      ],
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () { 
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text("OK"),
+                      ),
+                    ],
+                  ),
+                );
+              }, 
+              child: Text("Register Account")
+              ),
+            ],
+          ),
+
+        ],
+      ),
+      
+    );
+  }
+
 }
---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final bool isRequired;
+  final bool isPassword;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.labelText,
+    this.isRequired = false, // Default to not required
+    this.isPassword = false, // Default to not a password field
+  });
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  // State for toggling password visibility, only used if it's a password field
+  late bool _obscureText;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.isPassword;
+  }
+
+   @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 8.0),
+      child: TextField(
+        controller: widget.controller,
+        obscureText: _obscureText,
+        enableSuggestions: false,
+        autocorrect: false,
+        decoration: InputDecoration(
+          border: UnderlineInputBorder(),
+          alignLabelWithHint: false,
+          filled: false,
+          // Use a RichText label to conditionally add the red asterisk
+          label: RichText(
+            text: TextSpan(
+              text: widget.labelText,
+              style: TextStyle(
+                color: Colors.grey[700],
+                fontSize: 16,
+              ),
+              children: widget.isRequired
+                  ? [
+                      const TextSpan(
+                        text: " *",
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ]
+                  : null,
+            ),
+          ),
+          // Conditionally add the visibility icon only for password fields
+          suffixIcon: widget.isPassword
+              ? IconButton(
+                  icon: Icon(
+                    _obscureText ? Icons.visibility : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _obscureText = !_obscureText;
+                    });
+                  },
+                )
+              : null,
+        ),
+        keyboardType: TextInputType.visiblePassword,
+        textInputAction: TextInputAction.done,
+      ),
+    );
+  }
+}
