6e3d678e176f710c764d5cd0991e0e1e55ec30b0
John Herron <johnr2000@charter.net>
2025-09-14 16:59:24 -0500

    Added a route

--- lib/core/routing.dart
+++ lib/core/routing.dart
@@ -5,7 +5,10 @@ import '../features/auth/presentation/sign_up_screen.dart';
 import '../features/profile/presentation/edit_profile_screen.dart';
 import '../features/profile/presentation/profile_screen.dart';
 
+import '../features/maintenance/presentation/maintenance_detail_screen.dart';
+
 import 'layout/app_layout.dart';
+
 // Optional placeholder screens until you build them
 class _HomeScaffold extends StatelessWidget {
   const _HomeScaffold();
@@ -34,21 +37,30 @@ class AppRoutes {
   static const signUp = '/signup';
   static const home = '/home';
 
-  static const assets  = '/assets';
+  static const assets = '/assets';
 
   static const profile = '/profile';
   static const profileEdit = '/profile/edit';
-  
+
+  static const maintenanceDetail = '/maintenance/detail';
+
   static final routes = <String, WidgetBuilder>{
     login: (_) => const LoginScreen(),
     signUp: (_) => const SignUpScreen(),
 
     // stubs:
-    home:        (_) => const _HomeScaffold(),   // see below
-    assets:      (_) => const _AssetsScaffold(),
+    home: (_) => const _HomeScaffold(), // see below
+    assets: (_) => const _AssetsScaffold(),
 
     // new screens:
     profile: (_) => const ProfileScreen(),
     profileEdit: (_) => const EditProfileScreen(),
+
+    AppRoutes.maintenanceDetail: (ctx) {
+      final args = ModalRoute.of(ctx)?.settings.arguments;
+      // Use MaintenanceDetailScreen only
+      if (args is String) return MaintenanceDetailScreen(assetId: args);
+      return const MaintenanceDetailScreen(assetId: 'asset_001');
+    },
   };
 }
